f6444865a0fc397183894370dfb027d5c4e6ba06
Harish Muhammad Nazief <harishmuhammadnazief44@gmail.com>
2013-12-01 14:13:10 +0700

    NearMe implementation

--- src/com/sharlocstudio/sharloc/NearMeFragment.java
+++ src/com/sharlocstudio/sharloc/NearMeFragment.java
@@ -1,5 +1,15 @@
 package com.sharlocstudio.sharloc;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParserException;
+
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,14 +19,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.sharlocstudio.sharloc.R;
-import com.sharlocstudio.sharloc.R.id;
-import com.sharlocstudio.sharloc.R.layout;
+import com.sharlocstudio.sharloc.model.Friends;
+import com.sharlocstudio.sharloc.model.User;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,8 +38,8 @@ public class NearMeFragment extends Fragment {
 	private MapView mMapView;
 	private GoogleMap googleMap;
 	private Bundle mBundle;
-	private double myLat = 0.0;
-	private double myLong = 0.0;
+	private double myLat;
+	private double myLong;
 	private LatLng myPosition = null;
 	SupportMapFragment sMapFragment;
 
@@ -53,6 +65,16 @@ public class NearMeFragment extends Fragment {
 
 		getMyLocation();
 
+		ArrayList<User> friendList = getFriendList();
+
+		if (friendList != null) {
+
+			ArrayList<User> nearestFriend = getNearestFriend(friendList);
+
+			if (nearestFriend != null) {
+				addMarker(nearestFriend);
+			}
+		}
 		return rootView;
 	}
 
@@ -74,9 +96,21 @@ public class NearMeFragment extends Fragment {
 	}
 
 	private void setUpMap() {
-		googleMap.addMarker(new MarkerOptions().position(
-				new LatLng(-6.55939485, 106.77322609)).title(
-				"Afrishal Priyandhana"));
+
+	}
+
+	private void addMarker(ArrayList<User> nearestFriend) {
+		for (int i = 0; i < nearestFriend.size(); i++) {
+			double friendLat = Double.parseDouble(nearestFriend.get(i)
+					.getLatitude());
+			double friendLong = Double.parseDouble(nearestFriend.get(i)
+					.getLongitude());
+
+			String name = nearestFriend.get(i).getName();
+
+			googleMap.addMarker(new MarkerOptions().position(
+					new LatLng(friendLat, friendLong)).title(name));
+		}
 	}
 
 	@Override
@@ -99,26 +133,75 @@ public class NearMeFragment extends Fragment {
 
 	private void getMyLocation() {
 		googleMap.setMyLocationEnabled(true);
+		getActivity();
 		LocationManager locationManager = (LocationManager) getActivity()
-				.getSystemService(getActivity().LOCATION_SERVICE);
+				.getSystemService(Context.LOCATION_SERVICE);
 		Criteria criteria = new Criteria();
 		String provider = locationManager.getBestProvider(criteria, true);
 		Location location = locationManager.getLastKnownLocation(provider);
 
-		if (location != null) {
-			myLat = location.getLatitude();
-			myLong = location.getLongitude();
+		LatLng myLastPosition = new LatLng(location.getLatitude(),
+				location.getLongitude());
+
+		googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+				myLastPosition, 16.0f));
+
+		if (googleMap.getMyLocation() != null) {
+			myLat = googleMap.getMyLocation().getLatitude();
+			myLong = googleMap.getMyLocation().getLongitude();
 
 			myPosition = new LatLng(myLat, myLong);
-
-			googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-					myPosition, 16.0f));
 		}
+
 	}
 
-	private float getDistance(double lat1, double lat2, double long1,
+	private ArrayList<User> getFriendList() {
+		InputStream is = null;
+		ArrayList<User> friendList = null;
+
+		try {
+			is = new BufferedInputStream(new FileInputStream(new File(
+					getActivity().getFilesDir() + "/" + Friends.FILE_NAME)));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			friendList = Friends.getFriendList(is);
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return friendList;
+	}
+
+	private ArrayList<User> getNearestFriend(ArrayList<User> friendList) {
+		ArrayList<User> nearestFriend = new ArrayList<User>();
+
+		for (int i = 0; i < friendList.size(); i++) {
+			
+			User friend = friendList.get(i);
+			
+			double friendLat = Double.parseDouble(friend.getLatitude());
+			double friendLong = Double.parseDouble(friend.getLongitude());
+
+			int distance = getDistance(myLat, myLong, friendLat, friendLong);
+			
+			if (distance <= 1500) {
+				nearestFriend.add(friend);
+			}
+
+		}
+		return nearestFriend;
+	}
+
+	private int getDistance(double lat1, double lat2, double long1,
 			double long2) {
-		
+
 		Location locationA = new Location("point A");
 		locationA.setLatitude(lat1 / 1e6);
 		locationA.setLongitude(long1 / 1e6);
@@ -127,6 +210,6 @@ public class NearMeFragment extends Fragment {
 		locationB.setLatitude(lat2 / 1e6);
 		locationB.setLatitude(long2 / 1e6);
 
-		return locationA.distanceTo(locationB);
+		return (int) Math.round(locationA.distanceTo(locationB));
 	}
 }
